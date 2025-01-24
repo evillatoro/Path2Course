3ce3c3d1ed98fc4b5729b4b65601ea0b814e0987
Edwin Villatoro <villatoroedwin1@gmail.com>
2025-01-24 12:58:04 -0500

    added Vue cli and debounce for smoother typing

---
+++ src/components/course.vue
@@ -0,0 +1,37 @@
+<template>
+    <div class="question">
+      <p>{{ item.question }}</p>
+      <button @click="idState.replyOpen = !idState.replyOpen">Reply</button>
+      <textarea
+        v-if="idState.replyOpen"
+        v-model="idState.replyText"
+        placeholder="Type your reply"
+      />
+    </div>
+  </template>
+
+<script>
+import { IdState } from 'vue-virtual-scroller'
+
+export default {
+  mixins: [
+    IdState({
+      // You can customize this
+      idProp: vm => vm.item.id,
+    }),
+  ],
+
+  props: {
+    // Item in the list
+    item: Object,
+  },
+
+  // This replaces data () { ... }
+  idState () {
+    return {
+      replyOpen: false,
+      replyText: '',
+    }
+  },
+}
+</script>
